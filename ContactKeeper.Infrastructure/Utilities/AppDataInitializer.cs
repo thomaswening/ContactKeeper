d406bb50f581a7c5fe60179c055d983a57dd2040
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 21:37:04 +0200

    add class to initialize the necessary directories needed for storing the contacts

---
+++ ContactKeeper.Infrastructure/Utilities/AppDataInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Serilog;
+
+namespace ContactKeeper.Infrastructure.Utilities;
+
+/// <summary>
+/// Initializes the application data.
+/// </summary>
+public static class AppDataInitializer
+{
+    /// <summary>
+    /// Initializes the application data.
+    /// </summary>
+    /// <param name="baseDirectory">The base directory to use for the application data.</param>
+    /// <param name="defaultFileName">The default file name to use for the application data.</param>
+    /// <returns>The full path to the application data file.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="baseDirectory"/> is <see langword="null"/>.</exception>
+    public static string Initialize(string baseDirectory, string defaultFileName = "contacts.json")
+    {
+        EnsureDirectoryExists(baseDirectory);
+        return Path.Combine(baseDirectory, defaultFileName);
+    }
+
+    private static void EnsureDirectoryExists(string baseDirectory)
+    {
+        ArgumentNullException.ThrowIfNull(baseDirectory);
+
+        if (!Directory.Exists(baseDirectory))
+        {
+            Directory.CreateDirectory(baseDirectory);
+        }
+    }
+}
