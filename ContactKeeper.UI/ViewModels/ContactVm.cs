618db1cc8438b3fe1b7ed66298eac335177cd434
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 18:38:44 +0200

    add view model that represents a contact

---
+++ ContactKeeper.UI/ViewModels/ContactVm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContactKeeper.UI.ViewModels;
+/// <summary>
+/// Represents a contact in the application and exposes properties for observation in the UI.
+/// </summary>
+public partial class ContactVm : ObservableObject
+{
+    public Guid Id { get; }
+
+    [ObservableProperty]
+    private string firstName = string.Empty;
+
+    [ObservableProperty]
+    private string lastName = string.Empty;
+
+    [ObservableProperty]
+    private string email = string.Empty;
+
+    [ObservableProperty]
+    private string phone = string.Empty;
+
+    public ContactVm(Guid id, string firstName, string lastName, string phone, string email)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Phone = phone;
+    }
+}
