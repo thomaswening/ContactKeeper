296733df22e47cf38453f16d7e3e731a51a2046e
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 20:31:59 +0200

    add interface for ContactRepository to abstract the interaction with the data layer

---
+++ ContactKeeper.Core/Interfaces/IContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ContactKeeper.Core.Models;
+
+namespace ContactKeeper.Core.Interfaces;
+
+/// <summary>
+/// Represents a repository for managing contacts.
+/// </summary>
+public interface IContactRepository
+{
+    Task<IEnumerable<Contact>> GetContactsAsync();
+    Task SaveContactsAsync(IEnumerable<Contact> contacts);
+}
