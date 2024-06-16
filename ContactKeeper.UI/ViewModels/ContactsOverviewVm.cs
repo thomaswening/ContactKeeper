5ff7a8ac28dd02678dd9df7c28f985c0397abf0f
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 18:39:19 +0200

    add view model for the ContactsOverview which exposes commands for adding, deleting and editing contacts

---
+++ ContactKeeper.UI/ViewModels/ContactsOverviewVm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ContactKeeper.UI.ViewModels;
+internal partial class ContactsOverviewVm : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableCollection<ContactVm> contacts = [];
+
+
+    [RelayCommand]
+    private void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand(CanExecute = nameof(IsContactNotNull))]
+    private void DeleteContact(ContactVm contact)
+    {
+        throw new NotImplementedException();
+
+    }
+
+    [RelayCommand(CanExecute = nameof(IsContactNotNull))]
+    private void EditContact(ContactVm contact)
+    {
+        throw new NotImplementedException();
+
+    }
+
+    private static bool IsContactNotNull(ContactVm? contact) => contact is not null;
+}
