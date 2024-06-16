ac4e20282a2ac7a333c19e2608c7ced0cec0cb68
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 18:31:22 +0200

    add view ContactsOverview to see list of current contacts

---
+++ ContactKeeper.UI/ViewModels/MainWindowVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContactKeeper.UI.ViewModels;
+internal partial class MainWindowVm : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableObject currentViewModel;
+}
---
+++ ContactKeeper.UI/Views/ContactsOverview.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContactKeeper.UI.Views;
+/// <summary>
+/// Interaction logic for ContactsOverview.xaml
+/// </summary>
+public partial class ContactsOverview : UserControl
+{
+    public ContactsOverview()
+    {
+        InitializeComponent();
+    }
+}
