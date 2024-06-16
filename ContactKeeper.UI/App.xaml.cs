bdb202b04643d1bc22f9491a1df5a3315534e409
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 18:20:30 +0200

    set up MaterialDesign, logging and initialization of MainWindow

--- ContactKeeper.UI/App.xaml.cs
+++ ContactKeeper.UI/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+
+using Serilog;
 
 namespace ContactKeeper.UI;
 /// <summary>
@@ -8,5 +11,31 @@ namespace ContactKeeper.UI;
 /// </summary>
 public partial class App : Application
 {
-}
+    private const string ApplicationName = "ContactKeeper";
 
+    public App()
+    {
+        InitializeLogger();
+    }
+
+    private static void InitializeLogger()
+    {
+        var logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ApplicationName, "logs");
+
+        if (!Directory.Exists(logPath))
+        {
+            Directory.CreateDirectory(logPath);
+        }
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(Path.Combine(logPath, "ContactKeeperUI-.log"), rollingInterval: RollingInterval.Day)
+            .WriteTo.Debug()
+            .CreateLogger();
+    }
+
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
+}
