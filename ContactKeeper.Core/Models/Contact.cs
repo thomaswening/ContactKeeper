e28f21e31d29eaa28a87626898e6c0d1d8f7962f
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 19:12:51 +0200

    add Contact model

---
+++ ContactKeeper.Core/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ContactKeeper.Core.Models;
+[method: JsonConstructor]
+public class Contact(Guid id, string firstName, string lastName, string phone, string email)
+{
+    public Guid Id { get; } = id;
+    public string FirstName { get; set; } = firstName;
+    public string LastName { get; set; } = lastName;
+    public string Email { get; set; } = email;
+    public string Phone { get; set; } = phone;
+
+    public Contact(string firstName, string lastName, string phone, string email) : this(Guid.NewGuid(), firstName, lastName, phone, email)
+    {
+    }
+}
