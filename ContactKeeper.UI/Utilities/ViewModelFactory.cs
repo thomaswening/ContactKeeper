ebf3ff9deb567ca5a434f421b552ee0c8b7a6f48
Thomas Wening <thomaswening94@gmail.com>
2024-06-16 21:25:49 +0200

    implement factory to create view models without having to carry around all the dependencies

---
+++ ContactKeeper.UI/Utilities/ViewModelFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+using ContactKeeper.Core.Interfaces;
+using ContactKeeper.UI.ViewModels;
+
+namespace ContactKeeper.UI.Utilities;
+
+internal class ViewModelFactory(IContactService contactService)
+{
+    public ContactsOverviewVm CreateContactsOverviewVm()
+    {
+        return new ContactsOverviewVm(contactService);
+    }
+}
