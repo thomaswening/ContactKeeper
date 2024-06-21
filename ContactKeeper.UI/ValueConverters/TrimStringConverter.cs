faf125d031bf94d8f6f9ea2b12436ecf28c1ffb0
Thomas Wening <thomaswening94@gmail.com>
2024-06-21 20:40:51 +0200

    trim entered strings in edit contact view

---
+++ ContactKeeper.UI/ValueConverters/TrimStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ContactKeeper.UI.ValueConverters;
+/// <summary>
+/// A value converter that trims a string.
+/// </summary>
+internal class TrimStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string str)
+        {
+            return str.Trim();
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
