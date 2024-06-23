be62e0d6ee13f54bd7fba36fa20d059c7bf03c9e
Thomas Wening <thomaswening94@gmail.com>
2024-06-23 20:51:34 +0200

    add methods to create info from contact and overwrite contact with info

--- ContactKeeper.Core/Utilities/ContactInfo.cs
+++ ContactKeeper.Core/Utilities/ContactInfo.cs
@@ -41,6 +41,24 @@ public class ContactInfo
         return new Contact(FirstName, LastName, Phone, Email);
     }
 
+    /// <summary>
+    /// Creates a new <see cref="ContactInfo"/> object from the provided <see cref="Contact"/> object.
+    /// </summary>
+    /// <param name="contact">The contact to create a contact information object from.</param>
+    /// <returns>A new <see cref="ContactInfo"/> object created from the provided contact.</returns>
+    public static ContactInfo FromContact(Contact contact)
+    {
+        ArgumentNullException.ThrowIfNull(contact, nameof(contact));
+
+        return new ContactInfo
+        {
+            FirstName = contact.FirstName,
+            LastName = contact.LastName,
+            Email = contact.Email,
+            Phone = contact.Phone
+        };
+    }
+
 
     /// <summary>
     /// Determines whether the provided <see cref="Contact"/> object matches the information in this <see cref="ContactInfo"/> object.
@@ -48,12 +66,35 @@ public class ContactInfo
     /// </summary>
     /// <param name="contact">The contact to compare against.</param>
     /// <returns>True if the contact matches the information in this object; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the contact is null.</exception>
     public bool IsMatch(Contact contact)
     {
+        ArgumentNullException.ThrowIfNull(contact, nameof(contact));
+
         return (FirstName is null || contact.FirstName.Equals(FirstName, StringComparison.OrdinalIgnoreCase)) &&
                (LastName is null || contact.LastName.Equals(LastName, StringComparison.OrdinalIgnoreCase)) &&
                (Email is null || contact.Email.Equals(Email, StringComparison.OrdinalIgnoreCase)) &&
                (Phone is null || contact.Phone.Equals(Phone, StringComparison.OrdinalIgnoreCase));
+    }
+
+    /// <summary>
+    /// Updates the provided <see cref="Contact"/> object with the information in this <see cref="ContactInfo"/> object.
+    /// Only the properties that are not null in this object are used for updating.
+    /// </summary>
+    /// <param name="contact">The contact to update.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the contact is null.</exception>
+    public void OverwriteOnto(Contact contact)
+    {
+        ArgumentNullException.ThrowIfNull(contact, nameof(contact));
+
+        if (FirstName is not null)
+            contact.FirstName = FirstName;
+        if (LastName is not null)
+            contact.LastName = LastName;
+        if (Email is not null)
+            contact.Email = Email;
+        if (Phone is not null)
+            contact.Phone = Phone;
     }
 }
 
