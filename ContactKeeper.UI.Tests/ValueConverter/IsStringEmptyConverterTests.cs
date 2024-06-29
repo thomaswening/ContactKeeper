e1afca84fca794447777202044ad26a93fa52eba
Thomas Wening <thomaswening94@gmail.com>
2024-06-29 20:45:34 +0200

    add tests for value converters

---
+++ ContactKeeper.UI.Tests/ValueConverter/IsStringEmptyConverterTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoBogus;
+
+using ContactKeeper.UI.ValueConverters;
+
+using NUnit.Framework;
+
+namespace ContactKeeper.UI.Tests.ValueConverter;
+
+[TestFixture]
+internal class IsStringEmptyConverterTests
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private IsStringEmptyConverter converter;
+    private CultureInfo culture;
+    private Type targetType;
+    private string parameter;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [SetUp]
+    public void SetUp()
+    {
+        converter = new IsStringEmptyConverter();
+        targetType = AutoFaker.Generate<Type>();
+        culture = CultureInfo.InvariantCulture;
+        parameter = string.Empty;
+    }
+
+    [Test]
+    public void Convert_WithNullValue_ReturnsUnsetValue()
+    {        
+        var result = converter.Convert(null!, targetType, parameter, culture);
+
+        Assert.That(result, Is.EqualTo(System.Windows.DependencyProperty.UnsetValue));
+    }
+
+    [Test]
+    public void Convert_WithNonStringValue_ReturnsUnsetValue()
+    {
+        var value = AutoFaker.Generate<int>();
+
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.EqualTo(System.Windows.DependencyProperty.UnsetValue));
+    }
+
+    [Test]
+    public void Convert_WithEmptyString_ReturnsTrue()
+    {
+        var value = string.Empty;
+
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    [TestCase(" ")]
+    [TestCase("a")]
+    public void Convert_WithNonEmptyString_ReturnsTrue(string value)
+    {
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    [TestCase("", false)]
+    [TestCase(" ", true)]
+    [TestCase("a", true)]
+    public void Convert_WithInvertParameter_ReturnsInvertedResult(string value, bool expectedResult)
+    {
+        var parameter = "invert";
+
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ ContactKeeper.UI.Tests/ValueConverter/TrimStringConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoBogus;
+using ContactKeeper.UI.ValueConverters;
+
+using NUnit.Framework;
+
+namespace ContactKeeper.UI.Tests.ValueConverter;
+internal class TrimStringConverterTests
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private TrimStringConverter converter;
+    private CultureInfo culture;
+    private Type targetType;
+    private string parameter;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [SetUp]
+    public void SetUp()
+    {
+        converter = new TrimStringConverter();
+        targetType = AutoFaker.Generate<Type>();
+        culture = CultureInfo.InvariantCulture;
+        parameter = string.Empty;
+    }
+
+    [Test]
+    public void Convert_WithNullValue_ReturnsNull()
+    {        
+        var result = converter.Convert(null!, targetType, parameter, culture);
+
+        Assert.That(result, Is.Null);
+    }
+
+    [Test]
+    public void Convert_WithNonStringValue_ReturnsValue()
+    {
+        var value = AutoFaker.Generate<int>();
+
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.EqualTo(value));
+    }
+
+
+    [Test]
+    [TestCase("", "")]
+    [TestCase(" ", "")]
+    [TestCase(" test", "test")]
+    [TestCase("test ", "test")]
+    [TestCase(" test ", "test")]
+    [TestCase("test", "test")]
+    public void Convert_WithStringValue_ReturnsValue(string value, string expected)
+    {
+        var result = converter.Convert(value, targetType, parameter, culture);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
