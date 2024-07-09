8a2040a5be9a76bece9e901d9340c629a9868d1f
Thomas Wening <thomaswening94@gmail.com>
2024-07-09 21:28:33 +0200

    implement an about section

---
+++ ContactKeeper.UI/ValueConverters/MailToHyperlinkConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ContactKeeper.UI.ValueConverters;
+
+/// <summary>
+/// Converts a string to a mailto hyperlink.
+/// </summary>
+internal class MailToHyperLinkConverter : IValueConverter
+{
+    /// <summary>
+    /// Converts a string to a mailto hyperlink.
+    /// </summary>
+    /// <param name="value">The email address.</param>
+    /// <param name="targetType">Not used.</param>
+    /// <param name="parameter">Not used.</param>
+    /// <param name="culture">Not used.</param>
+    /// <returns>A mailto hyperlink.</returns>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string email)
+        {
+            return $"mailto:{email}";
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ContactKeeper.UI/ViewModels/AboutSectionVm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ContactKeeper.UI.ViewModels;
+
+internal partial class AboutSectionVm : ObservableObject
+{
+    public static string ApplicationDescription => "ContactKeeper is a contact management application for Windows Desktop, " +
+        "designed to help users store and manage their contacts efficiently.";
+
+    public static string Version => "1.0.0";
+
+    public static string Developer => "Thomas Wening";
+
+    public static string Contact => "thomaswening94@gmail.com";
+
+    public static string GithubLink => "https://github.com/thomaswening/ContactKeeper";
+
+    public static string License => "The ContactKeeper project is licensed under the GNU General Public License (GPL) version 3.";
+}
